8e2f035924fa7bbc99621834c31cc2d6fe6fa515
itacir gabral <mu@localhost.localdomain>
2019-08-01 21:15:36 -0400

    coments

--- esNext/eradores/09_corrotina.js
+++ esNext/eradores/09_corrotina.js
@@ -8,7 +8,10 @@ const inc = (x = 0) => x + 1
 const it = co(inc)
 
 console.dir(it.next(0))
-console.dir(it.next(1))
 console.dir(it.next(7))
 console.dir(it.next(3))
 console.dir(it.next(-1))
+console.dir(it.next(3))
+console.dir(it.next(3))
+
+// é possível retornar uma promessa?
